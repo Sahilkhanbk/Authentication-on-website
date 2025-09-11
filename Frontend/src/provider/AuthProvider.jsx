0599fe59968a0e2146259f5a6b33b60aca25a2f3
sahil khan <139511550+Sahilkhanbk@users.noreply.github.com>
2025-09-12 00:44:58 +0530

    Update AuthProvider.jsx

--- Frontend/src/provider/AuthProvider.jsx
+++ Frontend/src/provider/AuthProvider.jsx
@@ -3,6 +3,15 @@ import {jwtDecode} from "jwt-decode";
 import { useCookies } from "react-cookie";
 export const AuthContext = createContext();
 
+
+
+
+
+
+
+
+
+
 export default function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [token, setToken] = useState(null);
@@ -55,4 +64,5 @@ export default function AuthProvider({ children }) {
   );
 
 }
+
 
