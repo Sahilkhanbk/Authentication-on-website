b96cb7d655f2b1044b1e9637c3d4fa5aac1379a6
sahil khan <139511550+Sahilkhanbk@users.noreply.github.com>
2025-11-29 20:40:19 +0530

    Update exampleControllers.ts

--- Backend/src/controllers/exampleControllers.ts
+++ Backend/src/controllers/exampleControllers.ts
@@ -8,7 +8,6 @@ export const getExample: RequestHandler = (req, res, next) => {
 export const getExampleData: RequestHandler = async (req, res, next) => {
   const { name, id }: IExampleData = req.body;
 
-
   try {
     const example = await Example.findOne({ name });
 
